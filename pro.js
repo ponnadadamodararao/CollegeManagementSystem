a07e2ee84714fc42eecc5a45757ed24fc096d251
ponnadadamodararao <113875604+ponnadadamodararao@users.noreply.github.com>
2023-05-07 16:57:40 +0530

    IntialCommit

---
+++ pro.js
@@ -0,0 +1,12 @@
+function display()
+{ 
+    alert("Welcome to my Site....")
+}
+function view()
+{
+    setTimeout(() =>{
+         var body = document.getElementById("id")
+        body.style.backgroundColor ="pink"
+    },2000);
+}
+
